5c79b11b38ae4253693cabbe6ffd5fa0f843a6de
yutongz <2518766602@qq.com>
2025-02-03 17:28:28 +0800

    Add new modules and enhance AttnSleep model architecture

--- models/model.py
+++ models/model.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 import copy
+from copy import deepcopy
 from einops import rearrange
 
 
@@ -169,7 +170,119 @@ class MRCNN_Improved(nn.Module):
         return self.fusion(x_concat)
 
 
+
 # -------------------- 改进的AttnSleep整体架构 --------------------
+class CausalConv1d(torch.nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        self.__padding = (kernel_size - 1) * dilation
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=self.__padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+
+    def forward(self, input):
+        result = super(CausalConv1d, self).forward(input)
+        if self.__padding != 0:
+            return result[:, :, :-self.__padding]
+        return result
+
+class PositionwiseFeedForward(nn.Module):
+    "Positionwise feed-forward network."
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        "Implements FFN equation."
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+class SublayerOutput(nn.Module):
+    '''
+    A residual connection followed by a layer norm.
+    '''
+
+    def __init__(self, size, dropout):
+        super(SublayerOutput, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x + self.dropout(sublayer(self.norm(x)))
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class LayerNorm(nn.Module):
+    "Construct a layer normalization module."
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+class TCE(nn.Module):
+    '''
+    Transformer Encoder
+
+    It is a stack of N layers.
+    '''
+
+    def __init__(self, layer, N):
+        super(TCE, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return self.norm(x)
+
+class EncoderLayer(nn.Module):
+    '''
+    An encoder layer
+
+    Made up of self-attention and a feed forward layer.
+    Each of these sublayers have residual and layer norm, implemented by SublayerOutput.
+    '''
+    def __init__(self, size, self_attn, feed_forward, afr_reduced_cnn_size, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer_output = clones(SublayerOutput(size, dropout), 2)
+        self.size = size
+        self.conv = CausalConv1d(afr_reduced_cnn_size, afr_reduced_cnn_size, kernel_size=7, stride=1, dilation=1)
+
+
+    def forward(self, x_in):
+        "Transformer Encoder"
+        query = self.conv(x_in)
+        x = self.sublayer_output[0](query, lambda x: self.self_attn(query, x_in, x_in))  # Encoder self-attention
+        return self.sublayer_output[1](x, self.feed_forward)
+
 class AttnSleep_Improved(nn.Module):
     def __init__(self):
         super().__init__()
